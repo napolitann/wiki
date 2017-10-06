271ce74a1aadc576dbeee226ff00deaa9a7d576d
OnLiU <jhs_onliu@naver.com>
2017-10-07 05:39:11 +0900

    re-write with the use of react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import './App.scss'
-
+import AppRoot from './AppRoot'
 import Home from './components/Home'
 
 export default (): JSX.Element => (
 	<Router>
-		<Route path = '/' component = {Home} />
+		<AppRoot>
+			<Route path = '/' component = {Home} />
+		</AppRoot>
 	</Router>
 )
---
+++ src/AppRoot.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+import './AppRoot.scss'
+
+export interface Props {
+	children: React.ReactNode
+}
+
+export default (props: Props) => (
+	<div id = 'App'>
+		{props.children}
+	</div>
+)
